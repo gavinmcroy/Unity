ee1b1805f6dfa9f97a1a80a2000b6d0e5f6ef6ff
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2021-01-17 20:54:01 -0500

    clarified

--- Combat-Sys/Assets/Script/Movement.cs
+++ Combat-Sys/Assets/Script/Movement.cs
@@ -4,7 +4,9 @@ namespace Script
 {
     public class Movement : MonoBehaviour
     {
-        private const int MoveSpeed = 1;
+        [SerializeField] private float moveSpeed = 5;
+        private float _xMove;
+        private float _yMove;
         private Rigidbody2D _rigidBody2D;
 
         private void Awake()
@@ -14,19 +16,49 @@ namespace Script
 
         private void Update()
         {
+            ReadInput();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
         }
 
         /// <summary>
-        /// This function does something nifty
+        /// Reads input from the Input Manager Axis. (1,0) is to right and (0,-1) is down. Data can only be either
+        /// -1, 0, or 1
         /// </summary>
-        /// <param name = "a"> This is a parameter named a </param>
-        /// <param name = "b"> This is a parameter named b </param>
+        /// 
         /// <returns>
-        /// The sum of two integers.
+        /// Nothing.
         /// </returns>
-        /// <c> Math.getMyName() </c>
-        private void MyGarbageFunction(int a, int b)
+        private void ReadInput()
         {
+            _xMove = Input.GetAxisRaw("Horizontal");
+            _yMove = Input.GetAxisRaw("Vertical");
+        }
+
+        /// <summary>
+        /// Determines which direction the player
+        /// is travelling with a pair of coordinates. For example (1,0) is to the right. The players velocity attribute
+        /// inside rigid body is updated therefore this method must be placed inside FixedUpdate in order to work.
+        /// Players diagonal position is normalized.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Nothing.
+        /// </returns>
+        private void Move()
+        {
+            var move = new Vector2(_xMove, _yMove);
+            if (move.magnitude * moveSpeed > moveSpeed)
+            {
+                _rigidBody2D.velocity = move.normalized * moveSpeed;
+            }
+            else
+            {
+                _rigidBody2D.velocity = moveSpeed * move;
+            }
         }
 
         /// <summary>
